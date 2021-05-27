75ccd8fd6652450c308d59c8838234f0f53c4ad7
ThePieroCV <84894374+ThePieroCV@users.noreply.github.com>
2021-05-27 01:32:07 -0500

    bonus

---
+++ DRL Installing/env_inst.bat
@@ -0,0 +1,1 @@
+conda install -c conda-forge jupyterlab jupyterlab_code_formatter ipywidgets -y & pip install black jupyterlab-topbar jupyter-resource-usage jupyterlab-system-monitor aquirdturtle_collapsible_headings jupyterlab-drawio
